08ac806b6beb44ae97afb7b6fc667245979e1c94
ShiriCodes <shiri.faust@gmail.com>
2025-08-04 21:05:46 +0300

    Add input for secret word and display masked word with underscores

---
+++ step_3_display_word.py
@@ -0,0 +1,4 @@
+word = input("Please enter a word:") #asking for a word input
+print("_ "*len(word)) #mirroring number of letters in said word
+guess = input("Guess a letter:") #asking for a guess input
+print(guess.lower()) #mirroring guess
